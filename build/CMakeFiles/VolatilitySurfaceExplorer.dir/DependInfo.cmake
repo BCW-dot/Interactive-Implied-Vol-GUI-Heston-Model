15e26f1be2f6b20f30f5f0e92cd8c54d9a6c51d3
@BCW-dot <benecarl13@gmail.com>
2025-03-27 11:19:56 +0100

    next commit

---
+++ build/CMakeFiles/VolatilitySurfaceExplorer.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/benedikt/c++_vizu_test/src/glad.c" "CMakeFiles/VolatilitySurfaceExplorer.dir/src/glad.c.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/src/glad.c.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/backends/imgui_impl_glfw.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/backends/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/backends/imgui_impl_glfw.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/backends/imgui_impl_opengl3.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/backends/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/backends/imgui_impl_opengl3.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/imgui.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/imgui_demo.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_demo.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/imgui_draw.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_draw.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/imgui_tables.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_tables.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/imgui/imgui_widgets.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/imgui/imgui_widgets.cpp.o.d"
+  "/home/benedikt/c++_vizu_test/src/main.cpp" "CMakeFiles/VolatilitySurfaceExplorer.dir/src/main.cpp.o" "gcc" "CMakeFiles/VolatilitySurfaceExplorer.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
